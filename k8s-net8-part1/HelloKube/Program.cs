1c135468f3a3c0df9de8449dc024ca2d9a61b14e
Ihar Maiseyeu <horssupport@gmail.com>
2024-02-25 16:30:10 +0100

    latest changes in hello kube program

--- k8s-net8-part1/HelloKube/Program.cs
+++ k8s-net8-part1/HelloKube/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, Kube!");
+﻿Console.WriteLine("Hello, Kube! v.1.2");
+Console.WriteLine("Starting background process");
+Console.WriteLine("Looking for a configuration in local file");
+
+var stepToRun = 0;
+
+const string configFileName = "/configuration/step.config";
+
+if (File.Exists(configFileName))
+{
+    stepToRun = int.Parse(File.ReadAllText(configFileName));
+    Console.WriteLine($"{stepToRun} steps were completed. Continue process.");
+    stepToRun++;
+}
+else
+{
+    Console.WriteLine("Configuration does not exists. Starting from 0 step");
+}
+
+while (true)
+{
+    Console.WriteLine($"Processing step: {stepToRun}");
+
+    // Do some work
+    Thread.Sleep(5000);
+    File.WriteAllText(configFileName, stepToRun.ToString());
+
+    stepToRun++;
+}
